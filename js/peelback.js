a99b36b4ab1e167f7603a27e95decc14649f7ead
kusumi1705 <sumisumiwimp517@gmail.com>
2021-01-14 18:50:34 +0900

    url変更

--- js/peelback.js
+++ js/peelback.js
@@ -4,7 +4,7 @@ $(function() {
     $('#mekuri').peelback({
         adImage: './image/mekuri.png', //下に隠す画像
         peelImage  : './image/peel-image.png', //めくる画像
-        clickURL: 'https://www.days365.co.jp/', //クリック時のリンク先
+        clickURL: 'https://www.google.com/?hl=ja', //クリック時のリンク先
         smallSize: 60, //最小サイズ
         bigSize: 220, //最大サイズ
         autoAnimate: true //自動アニメーション
